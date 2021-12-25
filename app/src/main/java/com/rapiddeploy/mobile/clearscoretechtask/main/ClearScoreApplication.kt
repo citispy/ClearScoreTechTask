c697c7323e7e93a9023cb8f06e462907ae419a09
Yusuf <yusuf.isaacs@rapiddeploy.com>
2021-12-25 17:43:41 +0200

    Move Application class

--- app/src/main/java/com/rapiddeploy/mobile/clearscoretechtask/main/ClearScoreApplication.kt
+++ app/src/main/java/com/rapiddeploy/mobile/clearscoretechtask/main/ClearScoreApplication.kt
@@ -1,4 +1,4 @@
-package com.rapiddeploy.mobile.clearscoretechtask
+package com.rapiddeploy.mobile.clearscoretechtask.main
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
