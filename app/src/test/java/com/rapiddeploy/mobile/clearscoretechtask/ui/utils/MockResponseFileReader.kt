736fdf20012df29f842e84d33a612a13845a2dcb
Yusuf <yusuf.isaacs@rapiddeploy.com>
2021-12-27 16:32:44 +0200

    Create MockResponseFileReader

---
+++ app/src/test/java/com/rapiddeploy/mobile/clearscoretechtask/ui/utils/MockResponseFileReader.kt
@@ -0,0 +1,14 @@
+package com.rapiddeploy.mobile.clearscoretechtask.ui.dashboard.utils
+
+import java.io.InputStreamReader
+
+class MockResponseFileReader (path: String) {
+
+    val content: String
+
+    init {
+        val reader = InputStreamReader(this.javaClass.classLoader?.getResourceAsStream(path))
+        content = reader.readText()
+        reader.close()
+    }
+}
